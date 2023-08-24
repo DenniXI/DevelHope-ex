a2f1d162d7a5f4489d899d8df23169bbfb3df660
Dennis <emanuele.glorioso@gmail.com>
2023-08-24 16:54:47 +0200

    esercizio 16

---
+++ javascript_esercizi/ex16/exercise/exercise.js
@@ -0,0 +1,12 @@
+// Class definition
+
+class Person{
+    constructor(firstname, lastname){
+        this.firstName = firstname;
+        this.lastName = lastname;
+    }
+}
+
+
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
