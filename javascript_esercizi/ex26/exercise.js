87dd7e013bca7b2634783b102cd6d2b434649c92
Dennis <emanuele.glorioso@gmail.com>
2023-08-30 18:18:55 +0200

    ex26

--- javascript_esercizi/ex26/exercise.js
+++ javascript_esercizi/ex26/exercise.js
@@ -1,5 +1,15 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+// function sum(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+function sum(...numeri) {
+    let risultato = 0;
+    for(const num of numeri){
+        risultato += num;
+    }
+    return risultato;
 }
 
 console.log(sum(1, 2, 3, 4, 5));
