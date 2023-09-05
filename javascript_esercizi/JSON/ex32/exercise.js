282ed1ac0a3c05c596d8dc24053f81e7f2937d25
Dennis <emanuele.glorioso@gmail.com>
2023-09-05 14:29:54 +0200

    ex32

--- javascript_esercizi/JSON/ex32/exercise.js
+++ javascript_esercizi/JSON/ex32/exercise.js
@@ -4,12 +4,10 @@ const person = {
   lastName: 'Rossi',
   age: 25,
 
-  toJSON(){
-    return { id: this.id, age: this.age}
-  }
+
 };
 
 
-const json = JSON.stringify(person);
+const json = JSON.stringify({id: person.id, age: person.age});
 
 console.log(json); // Should return: { id: 1, age: 25 }
