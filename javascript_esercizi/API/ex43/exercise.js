836f9dc8625a4479aa9caab848216d3c24a1f310
Dennis <emanuele.glorioso@gmail.com>
2023-09-13 20:54:33 +0200

    Esercizi dal 40 al 44

---
+++ javascript_esercizi/API/ex43/exercise.js
@@ -0,0 +1,24 @@
+//Uso i commenti per dividere il tutto in parti altrimenti mi confondo :P
+
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+//Funzione per salvare l'oggetto user
+function save(user) {
+  try{
+
+    //Convertimento dell'oggetto user in una stringa
+    const userJson = JSON.stringify(user);
+
+    //Salva la stringa Json nel localStorage con con la chiave "user"
+    localStorage.setItem("user", userJson);
+    console.log('User object saved to localStorage.');
+  } catch {
+    (error) => console.error(error)
+  }
+}
+
+save(user)
